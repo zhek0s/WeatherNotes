90c9b883a334fb1c44a975dc47f4b662383d0227
zhek0s <mzheke123@gmail.com>
2026-02-11 18:51:12 +0100

    new project setup

---
+++ WeatherNotesTests/WeatherNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesTests.swift
+//  WeatherNotesTests
+//
+//  Created by Евгений Корженевский on 11.02.2026.
+//
+
+import Testing
+@testable import WeatherNotes
+
+struct WeatherNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
